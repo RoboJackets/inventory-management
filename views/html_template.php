29a0964321d57bc95e18d4c1bf8bed57c44817e1
Ryan Strat <ryanstrat@users.noreply.github.com>
2014-06-12 22:21:07 -0400

    Copied in beginning of html templates

---
+++ views/html_template.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    
+    <title><?php echo $title; ?></title>
+    
+    <script src="//code.jquery.com/jquery-1.11.0.min.js"></script>
+    <script src="../js/query.js"></script>
+
+</head>
+<body>
+    
+    <?php
+        echo $bodyContent;
+    ?>
+            
+</body>        
+</html>
---
+++ views/searchArea.php
@@ -0,0 +1,10 @@
+<nav>
+    <!--Insert Buttons for search mode here-->
+    <div class="search-mode-button">
+        <h1>barcode</h1>
+    </div>
+    <form>
+        <input type="search" name="query"></input>
+        <input type="submit" name="submit-query"/>
+    </form>
+</nav>
