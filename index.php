87a6874eeb31962399ec9e39203e2b05eed03094
Ryan Strat <ryanstrat@users.noreply.github.com>
2014-06-20 10:35:17 -0400

    Fix 404 error on homepage
    Error was caused by improper usage of optional url parameters

--- index.php
+++ index.php
@@ -6,13 +6,17 @@ require 'Slim/Slim.php';
 
 $app = new \Slim\Slim();
 
+$app->get('/)', function() use ($app) {
+    $app->view();
+    $app->render('html.php', array('title'=>'Robojackets Inventory',
+                                   'mode'=>'barcode'));
+});
 
-$app->get('(/:mode)', function($mode = 'barcode') use ($app) {
+$app->get('/:mode', function($mode) use ($app) {
     $app->view();
     $app->render('html.php', array('title'=>'Robojackets Inventory',
                                    'mode'=>$mode));
 });
-
 
 
 $app->run();
