49f6f4980c88f11da6ef88b3777d0861503f5b56
Ryan Strat <ryanstrat@users.noreply.github.com>
2014-06-20 10:27:07 -0400

    Added Template for results pane
    Some stock values are still populated in html.php. These will be removed
    and replaced with php in a future commit.

--- templates/html.php
+++ templates/html.php
@@ -8,9 +8,10 @@
     <title><?php echo $title; ?></title>
     
     <!-- Bootstrap -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/bootstrap.css" rel="stylesheet">
     <link href="css/bootstrap-theme.min.css" rel="stylesheet">
     <link href="css/custom.css" rel="stylesheet">
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,700' rel='stylesheet' type='text/css'>
 
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -30,25 +31,57 @@
 <nav>
     <div class="container">
         <div class="row">
-            <div class="col-sm-12">
-                <div id="barcode" class="nav-mode mode-selected">
+            <div class="col-xs-12 margin-fix">
+                <div id="barcode" class="nav-mode <?php if($mode == 'barcode') echo 'mode-selected' ?>">
                     <span class="glyphicon glyphicon-barcode"></span>
                 </div>
-                <div id="bin" class="nav-mode">
+                <div id="bin" class="nav-mode <?php if($mode == 'bin') echo 'mode-selected' ?>">
                     <span class="glyphicon glyphicon-inbox"></span>
                 </div>
             </div>
         </div>
         <div class="row">
-            <div class="col-sm-12"></div>
+            <div class="col-sm-12">
                 <div class="input-group">
-                    <input type="text" id="txtSubmitQuery" class="form-control">
+                    <input type="text" id="txtSubmitQuery" class="form-control txt-lg">
                     <span class="input-group-btn">
-                        <button id="BtnSubmitQuery" class="btn btn-primary" type="button">Search</button>
+                        <button id="BtnSubmitQuery" class="btn btn-primary btn-lg" type="button"><span class="glyphicon glyphicon-search"></span></button>
                     </span>
                 </div>
             </div>
         </div>
     </div>
 </nav>
+<body id="results-pane">
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12 space"></div>
+        </div>
+        <div class="row">
+            <div class="col-xs-12">
+                <div class="panel panel-primary">
+                    <div class="panel-heading">
+                        <div class="part-location">
+                            C14<!--populated by php-->
+                        </div>
+                        <div class="part">
+                            <div class="part-name">
+                                Part Name <!--populated by php-->
+                            </div>
+                            <div class="part-num">
+                                1529bz9382 <!--populated by php-->
+                            </div>
+                        </div>
+                    </div>
+                    <div class="panel-body">
+                        <dl class="dl-horizontal">
+                            <!--<dt>Value</dt>
+                            <dd>24uf</dd> -->
+                        </dl>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
 </html>
