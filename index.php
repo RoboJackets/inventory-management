516b473985cbe063d3c1695240d1c5f9ce750bc3
jonathan <jjones646@gmail.com>
2014-06-20 17:32:36 -0400

    fix routing

--- index.php
+++ index.php
@@ -3,13 +3,19 @@
 require 'Slim/Slim.php';
 \Slim\Slim::registerAutoloader(); //Req'd since not using Composer
 
-
 $app = new \Slim\Slim();
 
 $app->get('/', function() use ($app) {
     $app->view();
     $app->render('html.php', array('title'=>'Robojackets Inventory',
                                    'mode'=>'barcode'));
+});
+
+$app->get('/:mode', function($mode) use ($app) {
+    $app->view();
+    $app->render('html.php', array('title'      =>  'Robojackets Inventory',
+                                   'mode'       =>  $mode,
+                                    ));
 });
 
 $app->get('/:mode/:location', function($mode, $location) use ($app) {
