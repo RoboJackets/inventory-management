fb770ecf1c12e0a020f7c8dffef86201d8861b43
Ryan Strat <ryanstrat@users.noreply.github.com>
2014-06-27 15:10:12 -0400

    Added Input fields for add part UI

--- js/add.js
+++ js/add.js
@@ -1,6 +1,18 @@
 $(document).ready(function(){
-    $('.card #btn-add-next').click(function() {
+    
+    // Animate to next card
+    $('.card .next').click(function() {
         $(this).parent().addClass('off-left',500);
         $(this).parent().next().removeClass('off-right',500);
+        
+        $('.steps .steps-highlighted').last().next().addClass('steps-highlighted');
+    });
+    
+    // Animate to previous card
+    $('.card .back').click(function() {
+        $(this).parent().addClass('off-right',500);
+        $(this).parent().prev().removeClass('off-left',500);
+        
+        $('.steps .steps-highlighted').last().removeClass('steps-highlighted');
     });
 });
