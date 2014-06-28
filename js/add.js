65f42900b47d88c0b60447763ae14653a8d7c569
Ryan Strat <ryanstrat@users.noreply.github.com>
2014-06-28 17:33:07 -0400

    Updated card sliding JS to enable jumping to any card
    Note: This change broke functionality that colored the step indicators.
    This will be fixed in the future.

--- js/add.js
+++ js/add.js
@@ -1,18 +1,46 @@
-$(document).ready(function(){
+function slideCard(card, direction, side) {
+    if (direction === "in") {
+        if (side === "right") {
+            card.switchClass("off-left", "off-right", 0);
+            card.removeClass("off-right", 500);
+        } else {
+            card.switchClass("off-right", "off-left", 0);
+            card.removeClass("off-left", 500);
+        }
+    } else {
+        if (side === "right") {
+            card.addClass("off-right", 500);
+        } else {
+            card.addClass("off-left", 500);
+        }
+    }
+};
+
+function toCard(targetCardID, currentCardID) {
+    if ($(currentCardID).nextAll(targetCardID).length !== 0) {
+        slideCard($(currentCardID), "out", "left");
+        slideCard($(targetCardID), "in", "right");
+    } else if ($(currentCardID).prevAll(targetCardID).length !== 0) {
+        slideCard($(currentCardID), "out", "right");
+        slideCard($(targetCardID), "in", "left");
+    }
     
-    // Animate to next card
-    $('.card .next').click(function() {
-        $(this).parent().addClass('off-left',500);
-        $(this).parent().next().removeClass('off-right',500);
-        
-        $('.steps .steps-highlighted').last().next().addClass('steps-highlighted');
+    return targetCardID;
+};
+
+
+$(document).ready(function() {
+    var currentCardID = "#add-part";
+    
+    $(".to-card").click(function() {
+        var target = $(this).attr("card");
+        console.log("target: " + target);
+        if(!(("#" + target) === currentCardID)) {
+            currentCardID = toCard("#" + target, currentCardID);
+        } else {
+            console.log("same card");
+        }
     });
     
-    // Animate to previous card
-    $('.card .back').click(function() {
-        $(this).parent().addClass('off-right',500);
-        $(this).parent().prev().removeClass('off-left',500);
-        
-        $('.steps .steps-highlighted').last().removeClass('steps-highlighted');
-    });
+    //TO DO: Add Coloring to Steps
 });
