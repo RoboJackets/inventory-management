c16401d5854ae62cfcafff9791750006cddbba6e
Jonathan <jjones646@gmail.com>
2014-07-12 01:55:51 -0400

    debugging

--- php/api.php
+++ php/api.php
@@ -76,7 +76,7 @@ function getStatement($mode) {
 
 // sql queries - needs 
 function sqlBarcode(){
-    return '"SELECT barcode AS PackageIDs,
+    return "SELECT barcode AS PackageIDs,
         parts.PART_NUM AS PartNum,
         barcode_lookup.added AS BarAdd,
         name AS PartName,
@@ -95,7 +95,7 @@ function sqlBarcode(){
             ON parts.PART_NUM=barcode_lookup.PART_NUM
             LEFT JOIN attributes 
                 ON barcode_lookup.PART_NUM=attributes.PART_NUM
-    WHERE barcode_lookup.barcode=(?)"';
+    WHERE barcode_lookup.barcode=?";
 }
 
 function sqlPart() {
