85616a712232ec3a16b5db37073fdffa28513675
Jonathan <jjones646@gmail.com>
2014-07-12 04:36:08 -0400

    debugging

--- php/api.php
+++ php/api.php
@@ -17,7 +17,7 @@ function SearchDB($mode, $search_input) {
             return htmlspecialchars(stripslashes(trim($search_input)));
         } // cleanup input */
     
-    $query = $CONN->prepare(sqlBarcode());
+    $query = $CONN->prepare('"' . sqlBarcode() . '"');
     
     if(!$query){
         echo "Error: Could not prepare query statement. (" . $query->errno . ") " . $query->error . "\n";
@@ -77,23 +77,7 @@ function getStatement($mode) {
 
 // sql queries - needs 
 function sqlBarcode(){
-    return "SELECT barcode AS PackageIDs, " .
-        "parts.PART_NUM AS PartNum, " .
-        "barcode_lookup.added AS BarAdd, " .
-        "name AS PartName, " .
-        "category AS PartCat, " .
-        "description AS PartDesc, " .
-        "datasheet AS PartSheet, " .
-        "location AS PartLocation, " .
-        "flag_error AS PartErr, " .
-        "status AS PartStatus, " .
-        "parts.updated AS PartUpdated, " .
-    "FROM barcode_lookup " .
-        "LEFT JOIN parts " .
-            "ON parts.PART_NUM=barcode_lookup.PART_NUM " .
-            "LEFT JOIN attributes " .
-                "ON barcode_lookup.PART_NUM=attributes.PART_NUM " .
-    "WHERE barcode_lookup.barcode=?";
+    return "SELECT barcode AS PackageIDs, parts.PART_NUM AS PartNum, barcode_lookup.added AS BarAdd, name AS PartName, category AS PartCat, description AS PartDesc, datasheet AS PartSheet, location AS PartLocation, flag_error AS PartErr, status AS PartStatus, parts.updated AS PartUpdated, FROM barcode_lookup LEFT JOIN parts  ON parts.PART_NUM=barcode_lookup.PART_NUM LEFT JOIN attributes  ON barcode_lookup.PART_NUM=attributes.PART_NUM WHERE barcode_lookup.barcode=?";
 }
 
 function sqlPart() {
