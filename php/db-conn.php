ffa4107f1fafc24dc63aa187bcc90ff0763f48ce
Jonathan <jjones646@gmail.com>
2014-07-11 22:38:52 -0400

    more sql statements. structuring for more generic db connections

---
+++ php/db-conn.php
@@ -0,0 +1,13 @@
+<?php
+// Initalizes the database connection. Include for all files that access db.
+
+// Include configuration file
+if(!isset($path)){ $path = $_SERVER['DOCUMENT_ROOT'].'/php/'; }                               
+if (file_exists($path . 'config.php')) { require $path . 'config.php'; }   // include initial login info
+else { exit(); };
+
+// Create connection (object oriented way)
+$CONN = new mysqli(HOST, USER, PASSWORD, DATABASE);
+
+if ( $CONN->connect_error)
+    trigger_error('Database connection failed: ' . $CONN->connect_error, E_USER_ERROR);
