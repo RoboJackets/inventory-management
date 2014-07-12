7fb6e30dee717b8d2c75831e63c09f6ed918e769
Jonathan <jjones646@gmail.com>
2014-07-12 01:58:52 -0400

    debugging

--- php/api.php
+++ php/api.php
@@ -22,7 +22,7 @@ function SearchDB($mode, $search_input) {
     echo "mode: " . $mode . "\n";
     echo "input: " . $search_input . "\n\n";
     
-    $query = $CONN->prepare(getStatement($mode));
+    $query = $CONN->prepare("SELECT barcode AS PackageIDs, parts.PART_NUM AS PartNum, barcode_lookup.added AS BarAdd, name AS PartName, category AS PartCat, description AS PartDesc, datasheet AS PartSheet, location AS PartLocation, flag_error AS PartErr, status AS PartStatus, parts.updated AS PartUpdated, attributes.attribute AS PartAtrbs,  attributes.value AS PartVals, attributes.priority AS PartPrty FROM barcode_lookup LEFT JOIN parts ON parts.PART_NUM=barcode_lookup.PART_NUM LEFT JOIN attributes ON barcode_lookup.PART_NUM=attributes.PART_NUM WHERE barcode_lookup.barcode=?");
     
     if(!$query){
         echo "Variable not set\n";
