1489021eb0e89cdf52d7c91e0a7e938ef4446811
Jonathan <jjones646@gmail.com>
2014-07-11 22:39:00 -0400

    more sql statements. structuring for more generic db connections

--- php/search.php
+++ php/search.php
@@ -1,16 +1,14 @@
 <?php
-
-/* 
- * The script that runs when a search is requested from the client using ajax
- */
+//  used to perform a search on the database. returns json data.
 
 if(!isset($path)){ $path = $_SERVER['DOCUMENT_ROOT'].'/php/'; } // make sure path is known
-if (file_exists($path . 'api.php')) { require_once $path . 'api.php'; } // include api functions
+require $path.'db-conn.php';
+require $path.'api.php';
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {   // begin searching if user input is given
-    $connection = Connect();
+if ($_SERVER["REQUEST_METHOD"] == "GET") {   // begin searching if user input is given
+// 
+    // function that searches the database and returns json array
+    echo SearchDB($_GET['mode'], $_GET['input']);
     
-    // function that searches the database and returns the results in an array
-    echo SearchDB($connection, $_POST['mode'], $_POST['input']);
-    mysqli_close($connection);  // close connection
+    mysqli_close($CONN);  // close connection
 }
