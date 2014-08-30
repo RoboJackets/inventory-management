495298d76e7d1155843679e46bc6f77166b90c10
Ryan Strat <ryanstrat@users.noreply.github.com>
2014-08-30 19:48:17 -0400

    Cleaning code from debugging

--- php/submit-part.php
+++ php/submit-part.php
@@ -10,43 +10,19 @@ $app->post('/add/submit', function() use ($app) {
     var_dump($data);
     
     $partNum = $data->part_num;
-    /*
-    $sql = "SELECT COUNT(*) FROM `parts` WHERE PART_NUM='" . $partNum . "'";
-    $result = $CONN->query($sql);
 
-    if (!$result) {
-        printf("Error: %s\n", mysqli_error($CONN));
-        return;
-    }
-    
-    $row = mysqli_fetch_array($result);
-
-    var_dump($row);
-    
-    $count = $row['COUNT(*)']
-    */
     if ($stmt = $CONN->prepare("SELECT COUNT(*) FROM `parts` WHERE part_num=?")) {
-        echo "Running Query";
-        /* bind parameters for markers */
+
         $stmt->bind_param("s", $partNum);
-
-        /* execute query */
         $stmt->execute();
-
-        /* bind result variables */
         $stmt->bind_result($count);
-
-        /* fetch value */
         $stmt->fetch();
-
-        /* close statement */
         $stmt->close();
     } else {
-        echo "Query Failed";
-        echo "Prepare failed: (" . $CONN->errno . ") " . $CONN->error;
+        echo "Prepare failed: (" . $CONN->errno . ") " . $CONN->error . "<br>";
+        $app->response->setStatus(500);
+        return;
     }
-    
-    var_dump($count);
 
     if ($count == 0){
         echo "Insert New Row\n";
