8dd0867cec9f5280ffca5252a0e202981cb54fd5
Ryan Strat <ryanstrat@users.noreply.github.com>
2014-08-30 23:21:24 -0400

    Implement writing part of a JSON Part object to the parts table

--- php/submit-part.php
+++ php/submit-part.php
@@ -5,15 +5,14 @@ $app->post('/add/submit', function() use ($app) {
     if(!isset($path)){ $path = $_SERVER['DOCUMENT_ROOT'].'/php/'; }
     require $path . 'db-conn.php';
 
-    $data = json_decode($_POST['data']);
-
     var_dump($data);
     
-    $partNum = $data->part_num;
+    $data = json_decode($_POST['data']);
+    $part = $data->parts[0];
+    //add code here that check to ensure only 1 part was sent / use a for each structure
 
     if ($stmt = $CONN->prepare("SELECT COUNT(*) FROM `parts` WHERE part_num=?")) {
-
-        $stmt->bind_param("s", $partNum);
+        $stmt->bind_param("s", $part->part_num);
         $stmt->execute();
         $stmt->bind_result($count);
         $stmt->fetch();
@@ -26,8 +25,24 @@ $app->post('/add/submit', function() use ($app) {
 
     if ($count == 0){
         echo "Insert New Row\n";
-        //Insert new row
-        //Insert
+        
+        if ($stmt = $CONN->prepare("INSERT INTO parts (part_num, part_name, category, description, datasheet, location)
+            Values (?,?,?,?,?,?)")){
+            $stmt->bind_param("s", $part->part_num);
+            $stmt->bind_param("s", $part->part_name);
+            $stmt->bind_param("s", $part->category);
+            $stmt->bind_param("s", $part->description);
+            $stmt->bind_param("s", $part->datasheet);
+            $stmt->bind_param("s", $part->location);
+            $stmt->execute();
+            $stmt->fetch();
+            $stmt->close();
+        } else {
+            echo "Prepare failed: (" . $CONN->errno . ") " . $CONN->error . "<br>";
+            $app->response->setStatus(500);
+            return;
+        }
+        
     } else {
         //Error out if part already exists
         printf("Error: Part %s already exists:", $partNum);
