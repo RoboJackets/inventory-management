a4f1a954a97c878b29525438138857f94ee864e1
Jonathan <jjones646@gmail.com>
2014-09-15 03:56:34 -0400

    testign part class

--- php/search.php
+++ php/search.php
@@ -6,7 +6,8 @@
 if(!isset($path)){ $path = $_SERVER['DOCUMENT_ROOT'].'/php/'; } // make sure path is known
 require $path.'db-conn.php';
 require $path.'api.php';
-
+require $path.'c_Part.php';
+/*
 if ($_SERVER["REQUEST_METHOD"] == "GET") {   // begin searching if user input is given
     
     // function that searches the database and returns json formatted results.
@@ -14,4 +15,20 @@ if ($_SERVER["REQUEST_METHOD"] == "GET") {   // begin searching if user input is
     echo SearchDB($_GET['mode'], $_GET['input']);
     
     mysqli_close($CONN);  // close connection
+}
+*/
+
+
+// testing some different methods of searching
+if ($_SERVER["REQUEST_METHOD"] == "GET") {   // begin searching if user input is given
+    
+    $part = new Part($_GET['input']);
+    
+    
+    
+    $part->findPartID();
+    $part->findAllBarcodes();
+    $part->findPartInfo();
+    $part->showResults();
+    
 }
