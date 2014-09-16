9df58da3fff81b3d5324de113bf70e6e9ae152db
Jonathan <jjones646@gmail.com>
2014-09-16 03:42:11 -0400

    testing

--- js/inventory.js
+++ js/inventory.js
@@ -46,7 +46,7 @@ $(document).ready(function(){
                         
                     $.get("/php/populate-result-panes.php", { "items": container_count }, function (containers) {
                         
-                        for ($i = 0; $i<$container_count; $i++)
+                        for ($i = 0; $i<$data.num_results; $i++)
                         {
                             $( 'body' ).append( containers );
                         }
