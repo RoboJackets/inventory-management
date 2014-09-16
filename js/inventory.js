174d06fee364f7bb53c52483e509eaa673720e43
Jonathan <jjones646@gmail.com>
2014-09-16 03:28:08 -0400

    testing

--- js/inventory.js
+++ js/inventory.js
@@ -53,19 +53,19 @@ $(document).ready(function(){
                     
                 });
 
-
-                console.log('hey\n\n');
                 $.each(data, function (index, object) {
                     
                     console.log(index + " => " + object + "\n");                        
                             
                         console.log(index + " => " + object);     
                         console.log(index.name);
+                        if (index === "parts") {
 
-                        $('#part-location-data').html(data.parts.location);
-                        $('#part-name-data').html(data.parts.name);
-                        $('#part-num-data').html("PN: " + data.parts.part_num + "  | Bags: " + data.parts.num_bags + "  | Qty: " + data.parts.total_qty);
-                    
+                            $('#part-location-data').html(data.parts.location);
+                            $('#part-name-data').html(data.parts.name);
+                            $('#part-num-data').html("PN: " + data.parts.part_num + "  | Bags: " + data.parts.num_bags + "  | Qty: " + data.parts.total_qty);
+                        
+                        }
                 });
                 
             }
