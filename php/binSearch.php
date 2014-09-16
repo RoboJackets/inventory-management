02c66de63137cb9cc64e62d86689f45358b77ae7
Jonathan <jjones646@gmail.com>
2014-09-16 01:39:05 -0400

    removing variable dumps

--- php/binSearch.php
+++ php/binSearch.php
@@ -8,11 +8,11 @@ require $path.'db-conn.php';
 require $path.'c_Part.php';
 require $path.'c_MultiPart.php';
 
-
+/*
 ini_set('display_errors',1);
 ini_set('display_startup_errors',1);
 error_reporting(-1);
-
+*/
 
 // begin searching if user input is given
 if ($_SERVER["REQUEST_METHOD"] == "GET")
