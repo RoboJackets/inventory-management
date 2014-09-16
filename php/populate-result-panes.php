c86a89075cfb7f9994a2d9daa798f0f154ef5f06
Jonathan <jjones646@gmail.com>
2014-09-16 02:32:44 -0400

    gettin there!

--- php/populate-result-panes.php
+++ php/populate-result-panes.php
@@ -1,14 +1,14 @@
 <?php
 
 if(!isset($path)){ $path = $_SERVER['DOCUMENT_ROOT']; } // make sure path is known
-require $path . 'templates/result-pane.html'
+require $path . 'templates/result-pane.html';
 
 // begin searching if user input is given
 if ($_SERVER["REQUEST_METHOD"] == "GET")
 {
     $count = $_GET['items'];
 
-    for ( $i=0 ; i<$count ; $i++ )
+    for ( $i=0 ; $i<$count ; $i++ )
     {
       readfile('templates/result-pane.html');
     }
