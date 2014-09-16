cc193099f0d8342931391ec43141ae52810fc9eb
Jonathan <jjones646@gmail.com>
2014-09-16 02:35:56 -0400

    gettin there!

--- php/populate-result-panes.php
+++ php/populate-result-panes.php
@@ -1,7 +1,7 @@
 <?php
 
 if(!isset($path)){ $path = $_SERVER['DOCUMENT_ROOT']; } // make sure path is known
-require $path . 'templates/result-pane.html';
+require $path . '/templates/result-pane.html';
 
 // begin searching if user input is given
 if ($_SERVER["REQUEST_METHOD"] == "GET")
