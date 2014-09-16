02df7d9bee845c2e47be8b91b978126cac49f2fb
Jonathan <jjones646@gmail.com>
2014-09-16 04:19:16 -0400

    testing

--- js/inventory.js
+++ js/inventory.js
@@ -55,16 +55,16 @@ $(document).ready(function(){
                 });
             },
             
-        });
-        
-                    $.each(data.parts, function (arg, obj){
+            complete: function(){
+
+                $.each(data.parts, function (arg, obj){
                 $('#part-location-data').html(obj.location);
                 $('#part-name-data').html(obj.name);
                 $('#part-num-data').html("PN: " + obj.part_num + "  | Bags: " + obj.num_bags + "  | Qty: " + obj.total_qty);
-            });
-        
-        
-        
+                
+                }); },
+        });
+
         $('#txtSubmitQuery').val('');
     });
     
