4b97dc820d255e80d13c85a7f597b3815d542c8c
Jonathan <jjones646@gmail.com>
2014-10-04 20:36:00 -0400

    added revision place, major work needs to be done on footer now for #35

--- templates/footer.php
+++ templates/footer.php
@@ -1,11 +1,13 @@
-<?php echo '
-<div class="container">
-    <div class="col-xs-9 col-sm-11">
-        <p>
-        <a href="">Inventory Home</a> | 
-        <a href="">Legal</a> | 
-        <a href="">Contact Us</a>
-        </p>
+<nav>
+    <div class="container">
+        <div class="col-xs-9 col-sm-11">
+            <p>
+            <a href="">Home</a> |
+            <a href="">Legal</a> |
+            <a href="">Contact Us</a> |
+            <a href="http://github.com/RoboJackets/inventory-management/issues/new">Report a Bug</a> |
+                Revision: Alpha 1.0
+            </p>
+        </div>
     </div>
-</div>
-';
+</nav>
