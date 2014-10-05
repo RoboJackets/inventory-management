1ce5aa2f6010d0fe26ab8fc73bdf2281740b9690
Jonathan <jjones646@gmail.com>
2014-10-04 23:08:46 -0400

    updating validation using new Database class query method

--- php/validate-pn.php
+++ php/validate-pn.php
@@ -3,22 +3,11 @@
 $app->post('/add/validate-pn', function() use ($app) {
     // Set Database credentials
     if(!isset($path)){ $path = $_SERVER['DOCUMENT_ROOT'].'/php/'; }
-    require $path . 'db-conn.php';
+    require $path . 'c_Database.php';
 
-    $partNum = $_POST['partNumber'];
+    $connection = New Database();
 
-    if ($stmt = $CONN->prepare("SELECT COUNT(*) FROM `parts` WHERE part_num=?")) {
-        $stmt->bind_param("s", $partNum);
-        $stmt->execute();
-        $stmt->bind_result($count);
-        $stmt->fetch();
-        $stmt->close();
-    } else {
-        echo "Prepare failed: (" . $stmt->errno . ") " . $stmt->error . "<br>";
-        $app->response->setStatus(500);
-        return;
-    }
+    $count = $connection->searchQuery("SELECT COUNT(*) FROM `parts` WHERE part_num=(?)", $_POST['partNumber']);
 
-    echo json_encode($count);
-
+    echo array_shift($count)['COUNT(*)'];
 });
