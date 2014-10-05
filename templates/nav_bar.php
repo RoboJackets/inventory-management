d5631e997f17f8da40139edc359bb900cea57f4d
Jonathan <jjones646@gmail.com>
2014-10-05 01:58:31 -0400

    changing add tab to point to adding parts route

--- templates/nav_bar.php
+++ templates/nav_bar.php
@@ -4,21 +4,27 @@
         <img src="img/robobuzz-header.svg" height="90px">
         </a>
     </div>
-    <div class="col-xs-9 col-sm-11">
-        <h2>
-            <?php echo $title; ?>
-        </h2>
-            <ul class="nav nav-tabs" role="tablist">
-                <li class="active"><a href="/">Search</a></li>
-                <li class="disabled"><a href="/">Add</a></li>
-                <li class="disabled"><a href="/">Reports</a></li>
-                <li class="disabled">
-                    <li class="nav navbar-nav navbar-right">
-                        <button type="button" class="btn btn-default btn-sm">
-                            <span class="glyphicon glyphicon-cloud"></span> Login
-                        </button>
-                    </li>
+    <div class="col-sm-11 col-md-11">
+        <div class="col-xs-10 col-sm-12">
+            <h2>
+                <?php echo $title; ?>
+            </h2>
+        </div>
+        <div class="col-sm-12 col-xs-12">
+            <div class="col-xs-10 col-sm-6">
+                <ul class="nav nav-tabs" role="tablist">
+                    <li class="active"><a href="/">Search</a></li>
+                    <li class="disabled"><a href="/add">Add</a></li>
+                    <li class="disabled"><a href="/">Reports</a></li>
+                </ul>
+            </div>
+            <div class="col-xs-2 col-sm-6">
+                <li class="nav navbar-nav navbar-right">
+                    <button type="button" class="btn btn-default btn-sm">
+                        <span class="glyphicon glyphicon-cloud"></span> Login
+                    </button>
                 </li>
-            </ul>
+            </div>
+        </div>
     </div>
 </div>
