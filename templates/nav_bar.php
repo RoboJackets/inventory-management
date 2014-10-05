4bae6962a3c58b4b2cd703f73a04d0e5545717a2
Jonathan <jjones646@gmail.com>
2014-10-04 20:36:47 -0400

    solid progress made on #16, still work to go

--- templates/nav_bar.php
+++ templates/nav_bar.php
@@ -1,26 +1,24 @@
-<?php echo '
 <div class="container">
     <div class="col-sm-1 col-xs-3">
-        <a href="http://localhost">
+        <a href="/">
         <img src="img/robobuzz-header.svg" height="90px">
         </a>
     </div>
     <div class="col-xs-9 col-sm-11">
-        <h2>';
-echo $title; echo '
+        <h2>
+            <?php echo $title; ?>
         </h2>
             <ul class="nav nav-tabs" role="tablist">
-                <li class="active"><a href="#">Search</a></li>
-                <li class="disabled"><a href="#">Add</a></li>
-                <li class="disabled"><a href="#">Reports</a></li>
-                <ul class="nav navbar-nav navbar-right">
-                    <button type="button" class="btn btn-default btn-sm">
-                        <a href="http://github.com/RoboJackets/inventory-management/issues/new">
-                            <span class="glyphicon glyphicon-flag"></span> Report a Bug
-                        </a>
-                    </button>
-                </ul>
+                <li class="active"><a href="/">Search</a></li>
+                <li class="disabled"><a href="/">Add</a></li>
+                <li class="disabled"><a href="/">Reports</a></li>
+                <li class="disabled">
+                    <li class="nav navbar-nav navbar-right">
+                        <button type="button" class="btn btn-default btn-sm">
+                            <span class="glyphicon glyphicon-cloud"></span> Login
+                        </button>
+                    </li>
+                </li>
             </ul>
     </div>
 </div>
-';
