306a669f78c9727485e7a7f4ed248f41172d20fa
Jonathan <jjones646@gmail.com>
2014-10-05 04:01:58 -0400

    added standard bootstrap fixed bottom footer, but needs lost of work for mobile support

--- templates/footer.php
+++ templates/footer.php
@@ -1,13 +1,18 @@
-<nav>
+<nav class="navbar navbar-default navbar-fixed-bottom" role="navigation">
     <div class="container">
-        <div class="col-xs-9 col-sm-11">
-            <p>
-            <a href="">Home</a> |
-            <a href="">Legal</a> |
-            <a href="">Contact Us</a> |
-            <a href="http://github.com/RoboJackets/inventory-management/issues/new">Report a Bug</a> |
-                Revision: Alpha 1.0
-            </p>
+        <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
+            <h6>
+                <a href="">Home</a>  |
+                <a href="">Legal</a>  |
+                <a href="">Contact Us</a>  |
+                <a href="http://github.com/RoboJackets/inventory-management/issues/new">Report a Bug</a>
+            </h6>
+        </div>
+        <div class="col-xs-6 col-sm-6 col-md-2 col-lg-2" align="left">
+           <h6>Revision: Alpha 1.0</h6>
+        </div>
+        <div class="col-xs-6 col-sm-6 col-md-4 col-lg-4" align="right">
+            <h6><a href="http://robojackets.org">RoboJackets</a> &#169 2014</h6>
         </div>
     </div>
 </nav>
