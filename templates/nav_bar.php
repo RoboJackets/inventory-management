f9e42a47addef456c5e240a327c7eeb262a4cdc5
Jonathan <jjones646@gmail.com>
2014-10-05 04:03:46 -0400

    enable the add parts tab and set grid params for issues #35 and #16

--- templates/nav_bar.php
+++ templates/nav_bar.php
@@ -1,30 +1,27 @@
 <div class="container">
-    <div class="col-sm-1 col-xs-3">
+
+    <div class="col-xs-3 col-sm-2 col-md-1 col-lg-1">
         <a href="/">
         <img src="img/robobuzz-header.svg" height="90px">
         </a>
     </div>
-    <div class="col-sm-11 col-md-11">
-        <div class="col-xs-10 col-sm-12">
-            <h2>
-                <?php echo $title; ?>
-            </h2>
+
+
+    <!--<div class="col-xs-8 col-sm-8 col-md-9 col-lg-9 col-sm-pull-8">-->
+
+        <div class="col-xs-9 col-sm-10 col-md-11 col-lg-11">
+            <h2><?php echo $title; ?></h2>
         </div>
-        <div class="col-sm-12 col-xs-12">
-            <div class="col-xs-10 col-sm-6">
-                <ul class="nav nav-tabs" role="tablist">
-                    <li class="active"><a href="/">Search</a></li>
-                    <li class="disabled"><a href="/add">Add</a></li>
-                    <li class="disabled"><a href="/">Reports</a></li>
-                </ul>
-            </div>
-            <div class="col-xs-2 col-sm-6">
-                <li class="nav navbar-nav navbar-right">
-                    <button type="button" class="btn btn-default btn-sm">
-                        <span class="glyphicon glyphicon-cloud"></span> Login
-                    </button>
-                </li>
-            </div>
+
+<!--<div class="clearfix visible-xs-block"></div>-->
+
+        <div class="col-xs-12 col-sm-5 col-md-11 col-lg-11">
+            <ul class="nav nav-tabs" role="tablist">
+                <li class="active"><a href="/">Search</a></li>
+                <li class="inactive"><a href="/add">Add</a></li>
+                <li class="disabled"><a href="/">Reports</a></li>
+            </ul>
         </div>
-    </div>
+
+    <!--</div>-->
 </div>
