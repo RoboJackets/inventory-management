d69e4c7a658822e44d74f743ba4b720a0390896e
Jonathan <jjones646@gmail.com>
2014-10-12 00:55:39 -0400

    fixed top menu bar error on issue #16.

--- templates/nav_bar.php
+++ templates/nav_bar.php
@@ -17,9 +17,9 @@
 
         <div class="col-xs-12 col-sm-5 col-md-11 col-lg-11">
             <ul class="nav nav-tabs" role="tablist">
-                <li class="active"><a href="/">Search</a></li>
-                <li class="inactive"><a href="/add">Add</a></li>
-                <li class="disabled"><a href="/">Reports</a></li>
+                <li class=<?php if($tab == 'default'){echo 'active';}else{echo 'inactive';} ?>><a href="/">Search</a></li>
+                <li class=<?php if($tab == 'add'){echo 'active';}else{echo 'inactive';} ?>><a href="/add">Add</a></li>
+                <li class="disabled"><a>Reports</a></li>
             </ul>
         </div>
 
