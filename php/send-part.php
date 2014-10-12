75f14c4d205d7066cb86b8a2c295496ee6f478b9
Jonathan <jjones646@gmail.com>
2014-10-12 00:46:38 -0400

    updating adding a part file

---
+++ php/send-part.php
@@ -0,0 +1,21 @@
+<?php
+
+$app->post('/add/submit', function () use ($app) {
+// Set Database credentials
+    if (!isset($path)) {
+        $path = $_SERVER['DOCUMENT_ROOT'] . '/php/';
+    }
+    require $path . 'c_Part.php';
+    require $path . 'c_Database.php';
+
+    $data = json_decode(file_get_contents('php://input'));
+    $db = New Database();
+
+    foreach ($data as $index => $part) {
+        foreach ($part as $index2 => $partObj) {
+            $entry = New Part($db, array('part' => $partObj));
+            $entry->addBags();
+            $entry->addAttributes();
+        }
+    }
+});
