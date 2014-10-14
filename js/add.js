02e2ffe184a57130071f163933fe1ff29b00c163
Jonathan <jjones646@gmail.com>
2014-10-14 02:18:02 -0400

    updating js for using the info from server on issue #25

--- js/add.js
+++ js/add.js
@@ -6,17 +6,25 @@ function slideCard($card, direction, side) {
         if (side === "right") {
             $card.switchClass("off-left", "off-right", 0);
             $card.removeClass("hidden");
-            $card.removeClass("off-right", 300, "swing", function(){$card.find("input.focus").focus();});
+            $card.removeClass("off-right", 300, "swing", function () {
+                $card.find("input.focus").focus();
+            });
         } else {
             $card.switchClass("off-right", "off-left", 0);
             $card.removeClass("hidden");
-            $card.removeClass("off-left", 300, "swing", function(){$card.find("input.focus").focus();});
+            $card.removeClass("off-left", 300, "swing", function () {
+                $card.find("input.focus").focus();
+            });
         }
     } else {
         if (side === "right") {
-            $card.addClass("off-right", 300, "swing", function(){$card.addClass("hidden");});
+            $card.addClass("off-right", 300, "swing", function () {
+                $card.addClass("hidden");
+            });
         } else {
-            $card.addClass("off-left", 300, "swing", function(){$card.addClass("hidden");});
+            $card.addClass("off-left", 300, "swing", function () {
+                $card.addClass("hidden");
+            });
         }
     }
 };
@@ -38,7 +46,7 @@ function toCard(targetCardID, currentCardID) {
 };
 
 function enableCard($cards) {
-    $cards.each(function(index){
+    $cards.each(function (index) {
         var stepIndex = $(this).index() + 1;
         $("ol.steps li:nth-child(" + stepIndex + ")").addClass("btn-enabled");
     });
@@ -47,8 +55,8 @@ function enableCard($cards) {
     $cards.next(".card").children(".back").addClass("btn-enabled");
 };
 
-function disableCard($cards) {  
-    $cards.each(function(index){
+function disableCard($cards) {
+    $cards.each(function (index) {
         var stepIndex = $(this).index() + 1;
         $("ol.steps li:nth-child(" + stepIndex + ")").removeClass("btn-enabled");
     });
@@ -75,14 +83,14 @@ function addInputField(id, readOnly, key, value) {
     if (value === undefined) value = "";
 
     $newRow = $(id + " tbody tr:last-child").clone().find("input").val("").end();
-    $newRow.find("input").attr("placeholder","");
+    $newRow.find("input").attr("placeholder", "");
 
     var $removeButton = $($.parseHTML('<span class="glyphicon glyphicon-remove"></span>'));
 
-    $removeButton.click(function(){
-        $(this).closest('tr').children('td').animate({ padding: 0 }).wrapInner('<div />').children().slideUp(200, function() { 
-            $(this).closest('tr').remove(); 
-            $(id + " table tbody tr").each(function(idx){
+    $removeButton.click(function () {
+        $(this).closest('tr').children('td').animate({padding: 0}).wrapInner('<div />').children().slideUp(200, function () {
+            $(this).closest('tr').remove();
+            $(id + " table tbody tr").each(function (idx) {
                 $(this).children().first().text(idx + 1);
             });
             if (id == "#add-attributes") {
@@ -101,7 +109,7 @@ function addInputField(id, readOnly, key, value) {
         });
     });
 
-    $newRow.find("input").on("change keyup paste focus", function() {
+    $newRow.find("input").on("change keyup paste focus", function () {
         if (id == "#add-attributes") {
             if (validateAddAttributes()) {
                 enableCard($("#barcode"));
@@ -121,7 +129,7 @@ function addInputField(id, readOnly, key, value) {
 
     $newRow.find("td:nth-child(2) input").val(key);
     $newRow.find("td:nth-child(3) input").val(value);
-    if (readOnly) $newRow.find("td:nth-child(2) input").attr({readonly: true, tabindex:-1});
+    if (readOnly) $newRow.find("td:nth-child(2) input").attr({readonly: true, tabindex: -1});
 
     if (readOnly) {
         $(id + " tbody tr td:nth-child(2) input:not([readonly])").first().parents("tr").before($newRow);
@@ -129,14 +137,14 @@ function addInputField(id, readOnly, key, value) {
         $(id + " tbody tr:last-child").before($newRow);
     }
 
-    $(id + " table tbody tr").each(function(idx){
+    $(id + " table tbody tr").each(function (idx) {
         $(this).children().first().text(idx + 1);
     });
 };
 
 function addAttributes(fields) {
     $("#add-attributes table tbody tr td:nth-child(2) input[readonly]").parents("tr").find("span.glyphicon-remove").click();
-    $.each(fields, function(index, value) {
+    $.each(fields, function (index, value) {
         addInputField("#add-attributes", true, value);
     })
 }
@@ -182,46 +190,46 @@ function validateEditDetails() {
 function validateAddAttributes() {
     var flag = true;
     $("#add-attributes table tbody tr input").parent().removeClass("has-error");
-    $("#add-attributes table tbody tr").each(function(index, value) {
+    $("#add-attributes table tbody tr").each(function (index, value) {
         $key = $(this).find("td:nth-child(2) input");
         $val = $(this).find("td:nth-child(3) input");
-        if(($key.val() == "")?!($val.val() == ""):($val.val() == "")) { //XNOR
+        if (($key.val() == "") ? !($val.val() == "") : ($val.val() == "")) { //XNOR
             flag = false;
         }
     });
 
-    $("#add-attributes table tbody tr input").each(function(index, value) {
-        if(!/^[^"\\]*$/.test($(this).val())) {
+    $("#add-attributes table tbody tr input").each(function (index, value) {
+        if (!/^[^"\\]*$/.test($(this).val())) {
             $(this).parent().addClass("has-error");
             flag = false;
         }
     });
-    
+
     flag = flag && $("#add-attributes table tbody tr").length > 1;
-    
+
     return flag;
 }
 
 function validateBarcode() {
     var flag = true;
     $("#barcode table tbody tr input").parent().removeClass("has-error");
-    $("#barcode table tbody tr").each(function(index, value) {
+    $("#barcode table tbody tr").each(function (index, value) {
         $key = $(this).find("td:nth-child(2) input");
         $val = $(this).find("td:nth-child(3) input");
-        if(($key.val() == "")?!($val.val() == ""):($val.val() == "")) { //XNOR
+        if (($key.val() == "") ? !($val.val() == "") : ($val.val() == "")) { //XNOR
             flag = false;
         }
     });
 
-    $("#barcode table tbody tr input").each(function(index, value) {
-        if(!/^\d*$/.test($(this).val())) {
+    $("#barcode table tbody tr input").each(function (index, value) {
+        if (!/^\d*$/.test($(this).val())) {
             $(this).parent().addClass("has-error");
             flag = false;
         }
     });
-    
+
     flag = flag && $("#barcode table tbody tr").length > 1;
-    
+
     return flag;
 }
 
@@ -251,9 +259,9 @@ function showToast(alertType, title, message) {
     hideToast();
     alertType = "alert-" + alertType;
     var $toast = $("#toast-alert")
-    .addClass(alertType);
+        .addClass(alertType);
     $toast.append("<strong>" + title + ': </strong><p style="display:inline">' + message + "</p>");
-    $toast.children(".hide-toast").click(function(){
+    $toast.children(".hide-toast").click(function () {
         $toast.removeClass(alertType);
         $toast.children(":not(button)").remove();
         $toast.hide();
@@ -268,26 +276,26 @@ function hideToast() {
 function submitData() {
     var attributes = [];
     var bags = [];
-    
+
     $attributes = $("#add-attributes table tr:not(:last-child)");
-    
-    $attributes.each(function(index){
+
+    $attributes.each(function (index) {
         attributes.push(new Attribute(
             $(this).find("td:nth-child(2) input").val(),
             $(this).find("td:nth-child(3) input").val(),
             $(this).children("td:nth-child(1)").text()
         ));
     });
-    
+
     $bags = $("#barcode table tr:not(:last-child)");
 
-    $bags.each(function(index){
+    $bags.each(function (index) {
         bags.push(new Bag(
             $(this).find("td:nth-child(2) input").val(),
             $(this).find("td:nth-child(3) input").val()
         ));
     });
-    
+
     var part = new Part(
         $('#partNumberInput').val(),
         $('#partNameInput').val(),
@@ -298,25 +306,31 @@ function submitData() {
         bags,
         attributes
     );
-    
+
     var parts = {
-        "parts":[part]
+        "parts": [part]
     };
-    
+
     var data = JSON.stringify(parts);
     $.post("/add/submit", data, "json")
-    .done(function(xhr){
-        showToast("success", "Bin " + part.location, "Part added successfully!");
-        resetPage();
-    })
-    .fail(function(xhr){
-        showToast("error", "Error", "Could not submit part. Error code: " + xhr.status);
-        console.log(result);
-    });
+        .done(function (xhr) {
+            result = $.parseJSON(xhr);
+
+            if (result.validation_code) {
+                showToast("danger", result.title, result.message);
+            } else {
+                showToast("success", result.title, result.message);
+                resetPage();
+            }
+
+        })
+        .fail(function (xhr) {
+            showToast("danger", "Error", "Could not submit part. Error code: " + xhr.status);
+        });
 }
 
-function resetPage(){
-    currentCardID = toCard("#add-part","#barcode");
+function resetPage() {
+    currentCardID = toCard("#add-part", "#barcode");
     $("#categoryInput").val("");
     $(".card table tr:not(:last-child) td span").click();
     $("input, textarea").val("");
@@ -325,24 +339,24 @@ function resetPage(){
 
 }
 
-$(document).ready(function() {
-    $(window).keydown(function(event){
-        if(event.keyCode == 13) {
+$(document).ready(function () {
+    $(window).keydown(function (event) {
+        if (event.keyCode == 13) {
             event.preventDefault();
             return false;
         }
     });
 
-    $(".to-card").click(function() {
+    $(".to-card").click(function () {
         if ($(this).hasClass("btn-enabled")) {
             var target = $(this).attr("card");
-            if(!(("#" + target) === currentCardID)) {
+            if (!(("#" + target) === currentCardID)) {
                 currentCardID = toCard("#" + target, currentCardID);
             }
         }
     });
 
-    $(".card .next").click(function() {
+    $(".card .next").click(function () {
         if ($(this).hasClass("fast-track")) {
             var target = "barcode";
             $(".card .next, ol.steps li").addClass("btn-enabled")
@@ -354,21 +368,21 @@ $(document).ready(function() {
         }
     });
 
-    $(".card .back").click(function() {
+    $(".card .back").click(function () {
         if ($(this).hasClass("btn-enabled")) {
             var target = $(this).parent().prev().attr("id");
             currentCardID = toCard("#" + target, currentCardID);
         }
     });
 
-    $("#partNumberInput").on("change keyup paste", function() {
+    $("#partNumberInput").on("change keyup paste", function () {
         if (allowedChars.test($(this).val())) {
             enableCard($("#edit-details"));
 
-            var query = {"partNumber":$(this).val()};
-            $.post("add/validate-pn", query, function(result) {
+            var query = {"partNumber": $(this).val()};
+            $.post("add/validate-pn", query, function (result) {
                 //result = $.parseJSON(result);
-                if (result>0) {
+                if (result > 0) {
                     $("#partNumberInput").parent().addClass("has-success");
                     enableFastTrack();
                     $("#partNumberInput").tooltip();
@@ -389,19 +403,19 @@ $(document).ready(function() {
         }
     });
 
-    $("#add-attributes tbody tr:last-child input").on("focus", function() {
+    $("#add-attributes tbody tr:last-child input").on("focus", function () {
         addInputField("#add-attributes");
         var index = $(this).parent().index() + 1;
         $(this).parents("tr").prev().find("td:nth-child(" + index + ") input").focus();
     });
-    
-    $("#barcode tbody tr:last-child input").on("focus", function() {
+
+    $("#barcode tbody tr:last-child input").on("focus", function () {
         addInputField("#barcode");
         var index = $(this).parent().index() + 1;
         $(this).parents("tr").prev().find("td:nth-child(" + index + ") input").focus();
     });
 
-    $("#edit-details input, #edit-details select, #edit-details textarea").on("change keyup paste", function() {
+    $("#edit-details input, #edit-details select, #edit-details textarea").on("change keyup paste", function () {
         if (validateEditDetails()) {
             enableCard($("#add-attributes"));
         } else {
@@ -409,7 +423,7 @@ $(document).ready(function() {
         }
     });
 
-    $("#edit-details select").change(function() {
+    $("#edit-details select").change(function () {
         var attributes = {
             capacitor: [
                 "Type",
@@ -477,18 +491,18 @@ $(document).ready(function() {
         addAttributes(attributes[$(this).val()]);
     });
 
-    $(".card form").find("input:last").keydown(function(event){
-        if(event.keyCode == 13 || event.keyCode == 9) {
+    $(".card form").find("input:last").keydown(function (event) {
+        if (event.keyCode == 13 || event.keyCode == 9) {
             $(this).parents(".card").find(".next").click();
             event.preventDefault();
             return false;
         }
     });
-    
-    $("#btn-confirm-submit").click(function(){
-        if($(this).hasClass("btn-enabled")){
+
+    $("#btn-confirm-submit").click(function () {
+        if ($(this).hasClass("btn-enabled")) {
             submitData();
         }
     });
-    
+
 });
