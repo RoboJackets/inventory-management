a4dec33909a417f9d7c177c88808a10863936f79
Jonathan <jjones646@gmail.com>
2014-10-15 01:36:09 -0400

    adding livesearch for searching partnumbers

---
+++ php/livesearch.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Jonathan
+ * Date: 10/15/2014
+ * Time: 12:39 AM
+ */
+
+$app->post('/livesearch', function () {
+
+    if (!isset($path)) {
+        $path = $_SERVER['DOCUMENT_ROOT'] . '/php/';
+    }
+    require $path . 'c_Database.php';
+
+    $input = (string)$_POST['input'];
+
+    $db = new Database();
+    $results = array();
+
+    if (strlen($input) > 0) {
+        $results = $db->searchQuery('SELECT part_num FROM PARTS WHERE part_num LIKE (?) LIMIT 10', '%' . $input . '%');
+    }
+
+    $return = array();
+    foreach($results as $index => $part_num) {
+        $return[] = $part_num['part_num'];
+    }
+
+    $return = json_encode($return);
+    echo $return;
+});
